2d03cce5e6b039a99f5a6f6273f032fa5595a561
HaKicha <ivankika.1999@gmail.com>
2019-03-18 19:54:36 +0200

    Created ContactContainer(Content hardcoded!) Added free-brands-svg-icons package for font awesome

--- src/components/App.js
+++ src/components/App.js
@@ -2,15 +2,16 @@ import React, { Component } from "react";
 import MainTopBar from "./MainTopBar";
 import Header from "./Header";
 import MainHeadSlider from "./MainHeadSlider";
+import ContentContainer from "./ContentContainer";
 
 class App extends Component {
     render() {
-       console.log(this.a);
         return (
             <div>
                 <MainTopBar/>
                 <Header/>
                 <MainHeadSlider/>
+                <ContentContainer/>
             </div>
         );
     }
