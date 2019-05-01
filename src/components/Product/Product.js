ee6cb1d7cb144fdfd7051460ff0cf7338048752c
HaKicha <ivankika.1999@gmail.com>
2019-05-01 15:54:19 +0300

    End of sprint II

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -6,6 +6,7 @@ import MainTopBar from "../public/MainTopBar";
 import Header from '../public/Header';
 import Description from "./Description";
 import CommentsContainer from "./CommentsContainer";
+import Footer from "../public/Footer";
 
 export default class Product extends React.Component {
 
@@ -21,6 +22,7 @@ render() {
                 <Description ProductID={this.props.match.params.id}/>
             </Container>
             <CommentsContainer ProductID={this.props.match.params.id}/>
+            <Footer/>
         </div>
     )
     }
