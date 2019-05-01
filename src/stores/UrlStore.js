1863a4383178bad1d9f5375e47a5c31e87186aee
HaKicha <ivankika.1999@gmail.com>
2019-05-01 13:23:55 +0300

    End of sprint II

--- src/stores/UrlStore.js
+++ src/stores/UrlStore.js
@@ -4,7 +4,8 @@ export  default class UrlStore{
     // MAIN_URL = 'http://localhost:1337';
     // MAIN_GRAPHQL_URI = 'http://localhost:1337/graphql';
     MAIN_URL = 'https://pure-chamber-16886.herokuapp.com';
-    MAIN_GRAPHQL_URI = 'https://pure-chamber-16886.herokuapp.com/ZW5kcG9pbnQK';
-    TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1YzkxZWYxYjMxMDA0YjMxN2Y0ZTkyMzIiLCJpYXQiOjE1NTU3MDAwMzMsImV4cCI6MTU1ODI5MjAzM30.zHx0NHVluH0whMQ7llu_r0fO08FPpALJT13_YA0Aii4';
+    MAIN_GRAPHQL_URI = 'https://pure-chamber-16886.herokuapp.com/graphql';
+    // TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1YzkxZWYxYjMxMDA0YjMxN2Y0ZTkyMzIiLCJpYXQiOjE1NTU3MDAwMzMsImV4cCI6MTU1ODI5MjAzM30.zHx0NHVluH0whMQ7llu_r0fO08FPpALJT13_YA0Aii4';
+    TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1YzkwZDhmMWVlZDAyZDAwMTcyMDAxMzEiLCJpYXQiOjE1NTY3MDYxNDksImV4cCI6MTU1OTI5ODE0OX0.cJCAa8_yhEw3JvcZS0yxlUvyAMvBl-n0Lg3zmD_y9wE';
 
 }
