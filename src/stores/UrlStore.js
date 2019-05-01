9a0e9ff47605fd1f18512da70e47e79c0e28a2c6
HaKicha <ivankika.1999@gmail.com>
2019-05-01 11:22:44 +0300

    End of sprint II

--- src/stores/UrlStore.js
+++ src/stores/UrlStore.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
 export  default class UrlStore{
-    MAIN_URL = 'http://localhost:1337';
-    MAIN_GRAPHQL_URI = 'http://localhost:1337/graphql';
-    HEROKU_URL = 'https://pure-chamber-16886.herokuapp.com';
-    HEROKU_URI = 'https://pure-chamber-16886.herokuapp.com/ZW5kcG9pbnQK';
+    // MAIN_URL = 'http://localhost:1337';
+    // MAIN_GRAPHQL_URI = 'http://localhost:1337/graphql';
+    MAIN_URL = 'https://pure-chamber-16886.herokuapp.com';
+    MAIN_GRAPHQL_URI = 'https://pure-chamber-16886.herokuapp.com/ZW5kcG9pbnQK';
     TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1YzkxZWYxYjMxMDA0YjMxN2Y0ZTkyMzIiLCJpYXQiOjE1NTU3MDAwMzMsImV4cCI6MTU1ODI5MjAzM30.zHx0NHVluH0whMQ7llu_r0fO08FPpALJT13_YA0Aii4';
 
 }
