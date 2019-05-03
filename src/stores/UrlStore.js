96fe3b4a978c572fa0ba2328ce0c0afc7a389cd0
HaKicha <ivankika.1999@gmail.com>
2019-05-03 14:12:57 +0300

    Pre-release

--- src/stores/UrlStore.js
+++ src/stores/UrlStore.js
@@ -3,9 +3,11 @@ import React from 'react';
 export  default class UrlStore{
     // MAIN_URL = 'http://localhost:1337';
     // MAIN_GRAPHQL_URI = 'http://localhost:1337/graphql';
-    MAIN_URL = 'https://pure-chamber-16886.herokuapp.com';
-    MAIN_GRAPHQL_URI = 'https://pure-chamber-16886.herokuapp.com/graphql';
+    // MAIN_URL = 'https://pure-chamber-16886.herokuapp.com';
+    // MAIN_GRAPHQL_URI = 'https://pure-chamber-16886.herokuapp.com/graphql';
     // TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1YzkxZWYxYjMxMDA0YjMxN2Y0ZTkyMzIiLCJpYXQiOjE1NTU3MDAwMzMsImV4cCI6MTU1ODI5MjAzM30.zHx0NHVluH0whMQ7llu_r0fO08FPpALJT13_YA0Aii4';
-    TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1YzkwZDhmMWVlZDAyZDAwMTcyMDAxMzEiLCJpYXQiOjE1NTY3MDYxNDksImV4cCI6MTU1OTI5ODE0OX0.cJCAa8_yhEw3JvcZS0yxlUvyAMvBl-n0Lg3zmD_y9wE';
+    MAIN_URL = 'http://178.20.159.199';
+    MAIN_GRAPHQL_URI = 'http://178.20.159.199/graphql';
+    TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1Y2NjMWQxYWI4MDY2MjE1NTMzZmM0MTUiLCJpYXQiOjE1NTY4ODA5MDYsImV4cCI6MTU1OTQ3MjkwNn0.pz4Ht7nm1ERWRsOkgDKJLjgfaPwSvtQiXu9hpClc6sU';
 
 }
